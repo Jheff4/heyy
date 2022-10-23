ca40ab816907515f362c32cdb4157cf42218475d
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-10-23 14:51:40 +0100

    created the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography, Box, InputBase, Badge, Avatar } from '@mui/material'
+import { Diversity1, Mail, Notifications } from '@mui/icons-material'
+import { styled } from '@mui/system'
+
+const StyledToolbar = styled(Toolbar) ({
+  display: 'flex',
+  justifyContent: 'space-between'
+})
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%'
+}));
+
+const Icons = styled(Box) ({
+  display: "flex",
+  gap: "20px",
+  alignItems: "center"
+});
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar position='sticky'>
+      <StyledToolbar>
+        <Typography variant="h6" sx= {{ display: {xs: 'none', sm: 'block' }}}>HEYY</Typography>
+        <Diversity1 sx= {{ display: {xs: 'block', sm: 'none' }}}/>
+        <Search><InputBase placeholder="search"/></Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{width: 30, height: 30}}/>
+        </Icons>
+      </StyledToolbar>
+    </AppBar>
   )
 }
 
