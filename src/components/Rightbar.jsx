8c8e4f2488c42ded10a1b25ddc96572d96420ce9
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-10-30 02:36:05 +0100

    completed the sidebar and feed; added a few components to the rightbar.

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,15 +1,97 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material'
+import { Avatar, AvatarGroup, Box, Divider, ImageList, ImageListItem, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material'
 
 const Rightbar = () => {
   return (
-    <Box
-      flex={2}
-      p={2}
-      sx={{ display: { xs: "none", sm: "block"} }}
-    >
-      <Box position="fixed">
-        <Typography variant='h6' fontWeight={100}>Online Friends</Typography>
+    <Box flex={2} p={2} sx={{ display: { xs: "none", md: "block"} }} >
+      <Box position="fixed" width={300}>
+        <Typography variant='h6' fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={7} sx={{mr: {md: 8, lg: 0}}}>
+          <Avatar alt="Remy Sharp" src="https://images.pexels.com/photos/1036622/pexels-photo-1036622.jpeg?auto=compress&cs=tinysrgb&w=600" />
+          <Avatar alt="Travis Howard" src="https://material-ui.com/static/images/avatar/1.jpg" />
+          <Avatar alt="Cindy Baker" src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=600" />
+          <Avatar alt="Agnes Walker" src="https://images.pexels.com/photos/3992656/pexels-photo-3992656.png?auto=compress&cs=tinysrgb&w=600" />
+          <Avatar alt="Trevor Henderson" src="https://images.pexels.com/photos/90764/man-studio-portrait-light-90764.jpeg?auto=compress&cs=tinysrgb&w=600" />
+          <Avatar alt="Trevor Henderson" src="https://images.pexels.com/photos/7533347/pexels-photo-7533347.jpeg?auto=compress&cs=tinysrgb&h=204&fit=crop&w=228&dpr=2" />
+          <Avatar alt="Trevor Henderson" src="https://material-ui.com/static/images/avatar/6.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/images/avatar/7.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/images/avatar/8.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/images/avatar/9.jpg" />
+        </AvatarGroup>
+        <Typography variant='h6' fontWeight={100} mt={2} mb={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5} sx={{mr: {md: 8, lg: 0}}}>
+          <ImageListItem>
+            <img
+              src="https://images.pexels.com/photos/2422280/pexels-photo-2422280.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://images.pexels.com/photos/7887257/pexels-photo-7887257.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://images.pexels.com/photos/1387037/pexels-photo-1387037.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant='h6' fontWeight={100} mt={2}>
+          Latest Conversations
+        </Typography>
+        <List sx={{ width: {md: '80%', lg: '100%'}, maxWidth: 300, bgcolor: 'background.paper'}}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="https://images.pexels.com/photos/1181391/pexels-photo-1181391.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=600" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Our weekly stand-up is today"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {' — Do I have permission to be absent? I have to pick up…'}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </List>
       </Box>
     </Box>
   )
