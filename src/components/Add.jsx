1f5b9e8e9de62e0d33869a0ba6ece0bc79a4c301
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-10-31 14:10:58 +0100

    completed the modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,6 +1,6 @@
-import { Box, Fab, Modal, styled, Tooltip, Typography } from '@mui/material'
+import { Avatar, Box, Button, ButtonGroup, Fab, Modal, Stack, styled, TextField, Tooltip, Typography } from '@mui/material'
 import React, { useState } from 'react'
-import {Add as AddIcon} from "@mui/icons-material"
+import {Add as AddIcon, DateRange, EmojiEmotions, Image, PersonAdd, VideoCameraBack} from "@mui/icons-material"
 
 const Add = () => {
   const [open, setOpen] = useState(false);
@@ -9,6 +9,13 @@ const Add = () => {
     display: "flex",
     alignItems: "center",
     justifyContent: "center"
+  })
+  
+  const UserBox = styled(Box)({
+    display: "flex",
+    alignItems: "center",
+    gap: 5,
+    margin: 5
   })
 
   return (
@@ -24,10 +31,42 @@ const Add = () => {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box width={400} height={200} borderRadius={5} bgcolor="whitesmoke">
+        <Box width={400} height={280} borderRadius={5} bgcolor="whitesmoke" padding={2}>
           <Typography variant='h6' color="gray" textAlign="center">
             Create post
           </Typography>
+          <UserBox>
+            <Avatar 
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=400" 
+              sx={{ width: 30, height: 30 }}
+            />
+            <Typography fontWeight={500} variant="span">
+              Etinosa Ogbevoen
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="filled-multiline-static"
+            multiline
+            rows={3}
+            placeholder="what's on your mind?"
+            variant="filled"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            disableElevation
+            variant="contained"
+            aria-label="Disabled elevation buttons"
+          >
+            <Button>POST</Button>
+            <Button variant="outlined" sx={{width: "100px"}}><DateRange /></Button>
+          </ButtonGroup>
         </Box>
       </StyledModal>
     </>
