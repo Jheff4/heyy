3005500c5a5ea6fa014894a1814225b3214d0216
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2022-10-31 12:56:15 +0100

    added a pop-up modal

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,15 +1,35 @@
-import { Fab, Tooltip } from '@mui/material'
-import React from 'react'
+import { Box, Fab, Modal, styled, Tooltip, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import {Add as AddIcon} from "@mui/icons-material"
 
 const Add = () => {
+  const [open, setOpen] = useState(false);
+
+  const StyledModal = styled(Modal)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  })
+
   return (
     <>
       <Tooltip title="Add" sx={{position: "fixed", bottom: 20, left: {xs: "calc(50% - 25px)", md: 30}}}>
-        <Fab color="primary" aria-label="add">
+        <Fab color="primary" aria-label="add" onClick={ e => setOpen(true) }>
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={ e => setOpen(false) }
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={200} borderRadius={5} bgcolor="whitesmoke">
+          <Typography variant='h6' color="gray" textAlign="center">
+            Create post
+          </Typography>
+        </Box>
+      </StyledModal>
     </>
   )
 }
